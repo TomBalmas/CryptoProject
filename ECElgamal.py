726c8227890837ccf60929c74e6eedccb11c41aa
Tom Balmas <tombalmas12@gmail.com>
2021-12-27 15:43:02 +0200

    added elgamal key delivery

--- ECElgamal.py
+++ ECElgamal.py
@@ -2,48 +2,47 @@ import sys
 from random import randint, randrange
 from hashlib import sha256 as hash
 import libnum
+from utils import curve, scalar_mult, point_add
 
-from utils import curve,scalar_mult,point_add
 
-
-def sign(msg,dA):
-
+def sign(msg, dA):
     s = 0
     r = 0
-    if (len(sys.argv)>1):
-        msg=(sys.argv[1])
+    if (len(sys.argv) > 1):
+        msg = (sys.argv[1])
     # Alice's key pair (dA,QA)
 
-    h=int(hash(msg.encode()).hexdigest(), 16)
+    h = int(hash(msg.encode()).hexdigest(), 16)
     while s == 0:
-        k = randint(0, curve.n-1)
+        k = randint(0, curve.n - 1)
         rpoint = scalar_mult(k, curve.g)
         r = rpoint[0] % curve.n
-        if r==0:
+        if r == 0:
             continue
-    # Bob takes m and (r,s) and checks
+        # Bob takes m and (r,s) and checks
         inv_k = libnum.invmod(k, curve.n)
-        s = (inv_k*(h+r*dA)) % curve.n
-    #print(f"Msg: {msg}\n\nAlice's private key={dA}\nAlice's public key={QA}\nk= {k}\n\nr={r}\ns={s}")
+        s = (inv_k * (h + r * dA)) % curve.n
+    # print(f"Msg: {msg}\n\nAlice's private key={dA}\nAlice's public key={QA}\nk= {k}\n\nr={r}\ns={s}")
     return r, s
 
 
 # To check signature
 
-def verify(msg,r,s,QA):
-    inv_s = libnum.invmod(s,curve.n)
+def verify(msg, r, s, QA):
+    inv_s = libnum.invmod(s, curve.n)
     c = inv_s
     h = int(hash(msg.encode()).hexdigest(), 16)
-    u1=(h*c) % curve.n
-    u2=(r*c) % curve.n
-    P = point_add(scalar_mult(u1,curve.g), scalar_mult(u2,QA))
+    u1 = (h * c) % curve.n
+    u2 = (r * c) % curve.n
+    P = point_add(scalar_mult(u1, curve.g), scalar_mult(u2, QA))
     res = P[0] % curve.n
-    #print (f"\nResult r={res}")
-    if (res==r):
-	    print("Signature matches!")
+    # print (f"\nResult r={res}")
+    if (res == r):
+        print("Signature matches!")
     else:
         print("Not Valid signature!!")
         exit(0)
+
 
 def make_keypair():
     """Generates a random private-public key pair."""
